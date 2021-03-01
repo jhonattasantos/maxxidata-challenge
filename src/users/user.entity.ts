650860a71887bb14b888c4715cfe8680296cd52b
Jhonatta Santos <jhonattasantoss@hotmail.com>
2021-03-01 03:08:18 -0300

    creates user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+@Unique(['email'])
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: true, type: 'varchar', length: 64 })
+  confirmationToken: string;
+
+  @Column({ nullable: true, type: 'varchar', length: 64 })
+  recoverToken: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
